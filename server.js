1b77954f51d08f49ba57e0676bab653a332f84ac
Saroj Swain <sarojkumarswain51@gmail.com>
2021-06-05 22:07:05 +0530

    port added

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -21,7 +23,7 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
