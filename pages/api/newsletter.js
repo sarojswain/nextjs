7ee6a6de931ca1fe528391e079782dd9d3823bda
Saroj Swain <sarojkumarswain51@gmail.com>
2021-06-06 16:51:13 +0530

    regidtration api added

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -7,7 +9,37 @@ function handler(req, res) {
       return;
     }
 
-    res.status(201).json({ message: "Signed up!" });
+    const newRegistration = { userEmail };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://saroj:B3hbGjOCulat2WWZ@node-sgx7x.mongodb.net/nextjs?retryWrites=true&w=majority",
+        { useUnifiedTopology: true }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("nextjs").insertOne(newRegistration);
+      newRegistration.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing newRegistration failed!" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully registered!", message: newRegistration });
   }
 }
 
